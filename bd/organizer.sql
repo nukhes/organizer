da21325e8f9a2d448674de9d53dd4492f1bf8410
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2024-11-10 00:49:10 +0000

    feat: banco de dados

---
+++ bd/organizer.sql
@@ -0,0 +1,22 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+    username TEXT NOT NULL, 
+    hash TEXT NOT NULL
+);
+
+CREATE TABLE tasks (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    details TEXT,
+    date DATE,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE habits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    streak INTEGER NOT NULL,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+);
