419cfc43c05bb17600f1de238bba3b751e1e585f
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2024-11-12 18:47:17 +0000

    feat: rota de login e refatoração na estrutura da rota de registro

--- src/app.py
+++ src/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect
+from model import *
+
 
 app = Flask(__name__)
 app.secret_key = "638eb4448251ca268a11cc831211ad07ecbbf3e109a0c15db2ce2202daeea4249314201fffdb0b3b3ef9df6fcebe3a3a1078311da1d5122df811a587278a99c99fe78adb16b09179f8fcc87e9da98cc0676350754ee7ce07f7f9ec5f08a40990"
@@ -7,45 +9,40 @@ app.secret_key = "638eb4448251ca268a11cc831211ad07ecbbf3e109a0c15db2ce2202daeea4
 def index():
     return render_template("tasks.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    # TODO
-    pass
+    if request.method == "POST":
+        form = {
+            "name": request.form.get("name").strip(),
+            "pass": request.form.get("password").strip(),
+        }
+        
+        out = User.login(form)
+        
+        if out != True:
+            flash(out)
+            return redirect("/login")
+        else: 
+            flash("Logged In")
+            return redirect("/")
+        
+    
+    return render_template("login.html")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         form = {
             "name": request.form.get("name"),
-            "pass": request.form.get("password"),
-            "again": request.form.get("passwordconfirmation")
+            "pass": request.form.get("pass"),
+            "again": request.form.get("again")
         }
         
-        error = ""
+        out = User.create(form)
         
-        try:
-            # Check if every value is assigned
-            for value in form.values():
-                if value == "":
-                    error = "Fill the form correctly"
-            
-            # If passwords arent the same
-            if form["pass"] != form["again"]:
-                error = "Passwords must be equal"
-                
-            # If passwords are small
-            elif len(form["pass"]) < 8:
-                error = "Password length must be 8 characters"
-            
-        except:
-            error = "Unknown Error"
-        
-        if error:
-            flash(error)
-            return redirect("/register")
-        
-        pass
-        
+        if out:
+            flash(out)
+        return redirect("/register")
     
     return render_template("register.html")
 
--- src/model.py
+++ src/model.py
@@ -1,13 +1,65 @@
 import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
 
-path = "../db/organizer.db"
-connection = sqlite3.connect(path)
-cursor = connection.cursor()
+path = "./database/organizer.db"
 
-class User:
-    def __init__(self, id):
-        self.id = id
-        self.name = cursor.execute("SELECT name FROM users WHERE id = ?", id)
-        self.hash = cursor.execute("SELECT hash FROM users WHERE id = ?", id)
+class Database:
+    def create(form):
+        pass
+            
+class User(Database):
+    def create(form):
+        connection = sqlite3.connect(path)
+        cursor = connection.cursor()
+        
+        out = ""
+        
+        try:
+            # Check if every value is assigned
+            for value in form.values():
+                if value == "":
+                    out = "Fill the form correctly"
+            
+            # If passwords arent the same
+            if form["pass"] != form["again"]:
+                out = "Passwords must be equal"
+                
+            # If passwords are small
+            elif len(form["pass"]) < 8:
+                out = "Password length must be 8 characters"
+        except:
+            out = "Unknown Error"
+            
+        if not out:
+            cursor.execute("INSERT INTO users(username, hash) VALUES (?, ?)", (form["name"], generate_password_hash(form["pass"])))
+            connection.commit()
+        return out
+    
+    def login(form):
+        connection = sqlite3.connect(path)
+        cursor = connection.cursor()
+        
+        out = ""
+        
+        try:
+            # Check if every value is assigned
+            for value in form.values():
+                if value == "": 
+                    out = "Fill the form correctly"
 
-    # TODO: CRUD Functions
+                # If passwords are small
+                elif len(form["pass"]) < 8:
+                    out = "Password length must be 8 characters"
+        except:
+            out = "Unknown Error"
+            
+        if not out:
+            res = cursor.execute("SELECT username FROM users WHERE username = ?", (form["name"],))
+            
+            if not res:
+                out = "This user doesnt exist"
+            else:
+                isright = cursor.execute("SELECT username, hash FROM users WHERE username = ? AND hash = ?", (form["name"], generate_password_hash(form["pass"])))
+                if isright:
+                    return True
+        return out
