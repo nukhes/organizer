1a90f2b0006cba697ac429f2437930600330feaa
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2024-11-13 15:15:05 -0300

    fix: verificação para registar o usuário

--- src/model.py
+++ src/model.py
@@ -19,18 +19,23 @@ class User(Database):
             for value in form.values():
                 if value == "":
                     out = "Fill the form correctly"
+                    return out
             
             # If passwords arent the same
             if form["pass"] != form["again"]:
                 out = "Passwords must be equal"
                 
             # If passwords are small
-            elif len(form["pass"]) < 8:
+            if len(form["pass"]) < 8:
                 out = "Password length must be 8 characters"
-        except:
-            out = "Unknown Error"
             
-        if not out:
+            # If there are some user with this username
+            if cursor.execute("SELECT username FROM users WHERE username = ?", (form["name"],)) != "":
+                out = "This username is already taken"
+        except:
+            out = "Server Error"
+            
+        if out == "":
             cursor.execute("INSERT INTO users(username, hash) VALUES (?, ?)", (form["name"], generate_password_hash(form["pass"])))
             connection.commit()
         return out
@@ -42,24 +47,31 @@ class User(Database):
         out = ""
         
         try:
+            cursor.execute("SELECT id FROM users WHERE username = ?", (form["name"],))
+            user_id = cursor.fetchone()[0] if not cursor.fetchone()[0] is None else -1
+            
             # Check if every value is assigned
             for value in form.values():
                 if value == "": 
                     out = "Fill the form correctly"
+                    return out
 
-                # If passwords are small
-                elif len(form["pass"]) < 8:
-                    out = "Password length must be 8 characters"
+            # If username is incorrect
+            if user_id == -1:
+                out = "This user doesnt exists"
+            
+            # If password is wrong
+            cursor.execute("SELECT hash FROM users WHERE id = ?", (user_id,))
+            hash_query = cursor.fetchone()[0]
+            if check_password_hash(hash_query, form["pass"]):
+                out = "Your password is wrong"
         except:
-            out = "Unknown Error"
+            out = "Server Error"
             
         if not out:
-            res = cursor.execute("SELECT username FROM users WHERE username = ?", (form["name"],))
+            cursor.execute("SELECT username, hash FROM users WHERE id = ? AND hash = ?", (user_id, generate_password_hash(form["pass"])))
+            user_authenticated = cursor.fetchone()
+            if user_authenticated:
+                return True
             
-            if not res:
-                out = "This user doesnt exist"
-            else:
-                isright = cursor.execute("SELECT username, hash FROM users WHERE username = ? AND hash = ?", (form["name"], generate_password_hash(form["pass"])))
-                if isright:
-                    return True
         return out
