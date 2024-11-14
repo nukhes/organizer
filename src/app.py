b6d8d9ef083d02e8304f32615e5160c596fea0de
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2024-11-14 20:14:48 -0300

    feat: adição da rota /tasks e ajustes nas outras rotas

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, redirect, session
 from model import *
+from helpers import *
 
 
 app = Flask(__name__)
@@ -7,7 +8,9 @@ app.secret_key = "638eb4448251ca268a11cc831211ad07ecbbf3e109a0c15db2ce2202daeea4
 
 @app.route("/")
 def index():
-    return render_template("tasks.html")
+    if session["id"]:
+        return redirect("/tasks")
+    return redirect("/login")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -17,13 +20,16 @@ def login():
             "pass": request.form.get("password").strip(),
         }
         
-        out = User.login(form)
+        user = User()
+        out = user.login(form)
         
         if out == True:
-            flash("Logged In")
+            flash("Logged In", "success")
+            session["id"] = user.get_id(form["name"])
+            session["name"] = form["name"]
             return redirect("/")
         
-        flash(out)
+        flash(out, "error")
         return redirect("/login")
 
         
@@ -39,26 +45,32 @@ def register():
             "again": request.form.get("again")
         }
         
-        out = User.create(form)
+        user = User()
+        out = user.create(form)
         
         if out:
-            flash(out)
+            flash(out, "error")
         return redirect("/register")
     
     return render_template("register.html")
 
 
 @app.route("/tasks")
+@login_required
 def tasks():
-    # TODO
-    pass
+    if request.method == "POST":
+        pass
+    
+    return render_template("tasks.html", username=session["name"])
 
 @app.route("/habits")
+@login_required
 def habits():
     # TODO
     pass
 
 @app.route("/pomodoro")
+@login_required
 def pomodoro():
     # TODO
     pass
--- src/model.py
+++ src/model.py
@@ -4,74 +4,89 @@ from werkzeug.security import generate_password_hash, check_password_hash
 path = "./database/organizer.db"
 
 class Database:
+    def get_id():
+        pass
+    
     def create(form):
         pass
             
 class User(Database):
-    def create(form):
+    def get_id(self, name):
+        # Getting User ID
+        connection = sqlite3.connect(path)
+        cursor = connection.cursor()
+        cursor.execute("SELECT id FROM users WHERE username = ? LIMIT 1", (name,))
+        user_data = cursor.fetchone()
+        user_id = user_data[0] if user_data else -1
+        connection.close()
+        return user_id
+    
+    def create(self, form):
         connection = sqlite3.connect(path)
         cursor = connection.cursor()
         
         out = ""
         
-        try:
-            # Check if every value is assigned
-            for value in form.values():
-                if value == "":
-                    out = "Fill the form correctly"
-                    return out
+        user_id = self.get_id(form["name"])
+        
+        # Check if every value is assigned
+        for value in form.values():
+            if value == "":
+                out = "Fill the form correctly"
+                return out
+        
+        # If passwords arent the same
+        if form["pass"] != form["again"]:
+            out = "Passwords must be equal"
             
-            # If passwords arent the same
-            if form["pass"] != form["again"]:
-                out = "Passwords must be equal"
-                
-            # If passwords are small
-            if len(form["pass"]) < 8:
-                out = "Password length must be 8 characters"
-            
-            # If there are some user with this username
-            if cursor.execute("SELECT username FROM users WHERE username = ?", (form["name"],)) != "":
-                out = "This username is already taken"
-        except:
-            out = "Server Error"
+        # If passwords are small
+        if len(form["pass"]) < 8:
+            out = "Password length must be 8 characters"
+        
+        # If there are some user with this username
+        if user_id != -1:
+            out = "This username is already taken"
             
         if out == "":
             cursor.execute("INSERT INTO users(username, hash) VALUES (?, ?)", (form["name"], generate_password_hash(form["pass"])))
             connection.commit()
         return out
     
-    def login(form):
+    def login(self, form):
         connection = sqlite3.connect(path)
         cursor = connection.cursor()
         
         out = ""
         
         try:
-            cursor.execute("SELECT id FROM users WHERE username = ?", (form["name"],))
-            user_id = cursor.fetchone()[0] if not cursor.fetchone()[0] is None else -1
+            user_id = self.get_id(form["name"])
             
             # Check if every value is assigned
             for value in form.values():
-                if value == "": 
+                if value == "":
                     out = "Fill the form correctly"
                     return out
 
             # If username is incorrect
             if user_id == -1:
-                out = "This user doesnt exists"
-            
-            # If password is wrong
-            cursor.execute("SELECT hash FROM users WHERE id = ?", (user_id,))
-            hash_query = cursor.fetchone()[0]
-            if check_password_hash(hash_query, form["pass"]):
-                out = "Your password is wrong"
+                out = "This user doesn't exist"
+            else:
+                # If password is wrong
+                cursor.execute("SELECT hash FROM users WHERE id = ? LIMIT 1", (user_id,))
+                hash_query = cursor.fetchone()[0]
+                
+                ### DEBUG
+                print(hash_query)
+                print(form["pass"])
+                print(f"status: {(check_password_hash(hash_query, form['pass']))}")
+                ### DEBUG END
+                
+                
+                if not check_password_hash(hash_query, form["pass"]):
+                    out = "Your password is wrong"
+
+                connection.close()
         except:
-            out = "Server Error"
+            out = "Server error"
             
-        if not out:
-            cursor.execute("SELECT username, hash FROM users WHERE id = ? AND hash = ?", (user_id, generate_password_hash(form["pass"])))
-            user_authenticated = cursor.fetchone()
-            if user_authenticated:
-                return True
-            
-        return out
+        return True if not out else out
