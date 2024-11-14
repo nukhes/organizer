3dd899bd0b11e9855c6ec70c7c8326d3bb699451
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2024-11-14 20:17:05 -0300

    feat: o tema escolhido fica salvo em um cookie no client

--- src/static/js/theme.js
+++ src/static/js/theme.js
@@ -1,22 +1,24 @@
-import { getCookie } from "./cookie";
+import { getCookie } from "./cookie.js";
 
 const toggleButton = document.querySelector("#toggle_theme")
 const themeCss = document.querySelector("#theme_css")
 const themePath = "../static/css/themes/"
-
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
 
-if (getCookie("theme") != "") {
-    setTheme(getCookie("theme"));
-} else if (prefersDarkScheme.matches) {
-    setTheme("dark")
-} else {
-    setTheme("white")
+switch (getCookie("theme")) {
+    case "dark":
+        setTheme("dark")
+        break
+    case "light":
+        setTheme("light")
+        break
+    default:
+        if (prefersDarkScheme.matches) { setTheme("dark") } else { setTheme("white") }
 }
 
 // Toggle Theme when button is clicked
 toggleButton.addEventListener("click", () => {
-    if (themeCss.getAttribute("href") == `${themePath}light.css`) {
+    if (themeCss.getAttribute("href") === `${themePath}light.css`) {
         setTheme("dark");
     } else {
         setTheme("light");
@@ -24,19 +26,19 @@ toggleButton.addEventListener("click", () => {
 }) 
 
 function setTheme(theme) {
-    document.cookie = `theme=${theme}`
-    
     switch (theme) {
         case "dark":
             themeCss.href = `${themePath}dark.css`
             toggleButton.innerHTML = '<i class="fa-solid fa-sun"></i>'
+            document.cookie = `theme=${theme};`
             break
-        case "white":
+        case "light":
             themeCss.href = `${themePath}light.css`
             toggleButton.innerHTML = '<i class="fa-solid fa-moon"></i>'
+            document.cookie = `theme=${theme};`
             break
         default:
-            document.cookie = `theme=`
-            alert("Invalid Theme, this is probaly a bug, open an issue in GitHub.")
+            document.cookie = `theme=;`
+            alert("Invalid Theme, this is probaly a bug, open an issue in GitHub." + getCookie("theme"))
     }
 }
