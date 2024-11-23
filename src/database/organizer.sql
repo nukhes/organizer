9812a601e6e43ec400204eb2242b69d70e0b2386
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2024-11-23 18:31:28 -0300

    fix: status default da task setado para "0" ou "false"

--- src/database/organizer.sql
+++ src/database/organizer.sql
@@ -1,22 +1,22 @@
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-    username TEXT NOT NULL, 
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    username TEXT NOT NULL,
     hash TEXT NOT NULL
 );
-
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE habits (
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    user_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    streak INTEGER NOT NULL,
+    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
+);
 CREATE TABLE tasks (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     user_id INTEGER NOT NULL,
     name TEXT NOT NULL,
     details TEXT,
     date DATE,
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
-CREATE TABLE habits (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    user_id INTEGER NOT NULL,
-    name TEXT NOT NULL,
-    streak INTEGER NOT NULL,
+    status BOOL DEFAULT 0,
     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
 );
